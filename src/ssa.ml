f3e3f661b18b826b7ab543f168942123b4069977
Tobe O <thosakwe@gmail.com>
2019-12-21 08:28:26 -0500

    ssa stringification

--- src/ssa.ml
+++ src/ssa.ml
@@ -1,6 +1,10 @@
-type instr =
+type symbol =
+  | FuncSymbol of string * typ * (typ list) * (instr list)
+  | VarSymbol of string * typ
+  | TypeSymbol of typ
+and instr =
   | VarAssn of string * typ * value
-  | VarGet of string
+  | VarGet of string * typ
   | Return of typ * value
   | ReturnVoid
 and typ =
@@ -19,3 +23,36 @@ let type_of_value = function
   | IntLiteral _ -> IntType
   | DoubleLiteral _ -> DoubleType
   | BoolLiteral _ -> BoolType
+
+let rec string_of_symbol = function
+  | FuncSymbol (name, returns, params, instrs) ->
+    let param_string = String.concat ", " (List.map string_of_type params) in
+    "fn " ^ name ^ "(" ^ param_string ^ "): " ^ (string_of_type returns)
+    ^ " = \n" ^ (string_of_block instrs)
+  | TypeSymbol typ -> "type " ^ (string_of_type typ)
+  | VarSymbol (name, typ) -> name ^ ": " ^ (string_of_type typ)
+and string_of_block block =
+  let indented_string_of_instr instr =
+    "  " ^ (string_of_instr instr)
+  in
+  String.concat "\n" (List.map indented_string_of_instr block)
+and string_of_instr = function
+  | VarAssn (name, typ, value) ->
+    "set " ^ name ^ ": " ^ (string_of_type typ)
+    ^ " = " ^ (string_of_value value)
+  | VarGet (name, typ) -> "get " ^ name ^ ": " ^ (string_of_type typ)
+  | Return (typ, value) -> "return " ^ (string_of_type typ) ^ " " ^ (string_of_value value)
+  | ReturnVoid -> "return void"
+and string_of_type = function
+  | IntType -> "int"
+  | DoubleType -> "double"
+  | BoolType -> "bool"
+  | VoidType -> "void"
+and string_of_value = function
+  | IntLiteral v -> string_of_int v
+  | DoubleLiteral v -> string_of_float v
+  | BoolLiteral v -> string_of_bool v
+  | FunctionCall (_, target, args) ->
+    let target_string = string_of_value target in
+    let arg_string = String.concat ", " (List.map string_of_value args) in
+    target_string ^ "(" ^ arg_string ^ ")"
