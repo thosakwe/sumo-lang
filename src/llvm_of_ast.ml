a15b46f5f19cee9a0e15e7d6f089af9fb319bf6e
Tobe O <thosakwe@gmail.com>
2019-12-21 14:59:16 -0500

    Valid modules in llvm_of_ast

--- src/llvm_of_ast.ml
+++ src/llvm_of_ast.ml
@@ -3,4 +3,5 @@ let compile_single_ast path c_unit =
   let llvm_context = Llvm_of_ssa.compile_universe path ssa_context.errors universe in
   Ssa.dump_universe universe;
   Llvm.dump_module llvm_context.llvm_module;
+  Llvm_analysis.assert_valid_module llvm_context.llvm_module;
   ()
