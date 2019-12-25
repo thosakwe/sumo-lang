2241da6b995d6f6717432483c22b7a4ed737d392
Tobe O <thosakwe@gmail.com>
2019-12-24 20:48:28 -0500

    Compile AST blocks to new blocks

--- src/namer.ml
+++ src/namer.ml
@@ -5,10 +5,10 @@ type t = int StringMap.t
 let empty = StringMap.empty
 
 let next_name name namer =
-  if not (Scope.mem name namer) then
-    (name, Scope.add name 1 namer)
+  if not (StringMap.mem name namer) then
+    (name, StringMap.add name 1 namer)
   else
-    let current = Scope.find name namer in
+    let current = StringMap.find name namer in
     let new_name = name ^ (string_of_int current) in
     let new_count = current + 1 in
-    (new_name, Scope.add name new_count namer)
+    (new_name, StringMap.add name new_count namer)
