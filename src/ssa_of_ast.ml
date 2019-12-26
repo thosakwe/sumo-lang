cbf0a2be6e26195de7588533a73ede63774ed408
Tobe O <thosakwe@gmail.com>
2019-12-25 23:35:54 -0500

    Optional casts

--- src/ssa_of_ast.ml
+++ src/ssa_of_ast.ml
@@ -882,7 +882,7 @@ and cast_value context span value_opt from_type to_type =
       (context, Ok (Some (OptionalNone inner)))
     | (_, (OptionalType inner), (Some value)) ->
       if from_type = inner then
-        (context, Ok (Some value))
+        (context, Ok (Some (OptionalSome (to_type, value))))
       else 
         failure
     | _ ->  failure
