5ab26938b07b11175fbb33095277df8d696e32c0
Tobe O <thosakwe@gmail.com>
2019-12-26 10:40:12 -0500

    Struct type -> llvm

--- src/llvm_of_ssa.ml
+++ src/llvm_of_ssa.ml
@@ -503,6 +503,10 @@ and compile_type context = function
     let struct_type = Llvm.struct_type context [| bool_type; llvm_inner |] in
     (* Llvm.pointer_type struct_type *)
     Llvm.pointer_type struct_type
+  | StructType (fields) ->
+    let llvm_fields = List.map (function (_, typ) -> compile_type context typ) fields in
+    let struct_type = Llvm.struct_type context (Array.of_list llvm_fields) in
+    Llvm.pointer_type struct_type
   (* Note: This case should never be reached. *)
   | UnknownType -> Llvm.void_type context
 
